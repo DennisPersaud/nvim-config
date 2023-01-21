e804094285474a458b27753d134daafbc155c14b
Dennis Persaud <dpersaud247@gmail.com>
2023-01-21 16:59:47 -0500

    Fixed autcomplete menu

--- lua/swim/lsp/cmp.lua
+++ lua/swim/lsp/cmp.lua
@@ -51,8 +51,13 @@ cmp.setup({
 			local kind = require("lspkind").cmp_format({ mode = "symbol_text", maxwidth = 50 })(entry, vim_item)
 			local strings = vim.split(kind.kind, "%s", { trimempty = true })
 			kind.kind = " " .. (strings[1] or "") .. " "
-			kind.menu = "    (" .. (strings[2] or "") .. ")"
-
+			kind.menu = ({
+				nvim_lsp = "[LSP]",
+				nvim_lua = "[LUA]",
+				luasnip = "[Snippet]",
+				buffer = "[Buffer]",
+				path = "[Path]",
+			})[entry.source.name]
 			return kind
 		end,
 	},
@@ -62,5 +67,9 @@ cmp.setup({
 		{ name = "luasnip" },
 		{ name = "buffer" },
 		{ name = "path" },
+	},
+	confirm_opts = {
+		behavior = cmp.ConfirmBehavior.Replace,
+		select = false,
 	},
 })
