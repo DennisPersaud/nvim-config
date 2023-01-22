5e2e349b77637973f1a6ec9b9e0b66a20d328198
Dennis Persaud <dpersaud247@gmail.com>
2023-01-21 19:23:13 -0500

    Fixed cmp mapping

--- lua/swim/lsp/cmp.lua
+++ lua/swim/lsp/cmp.lua
@@ -14,10 +14,7 @@ cmp.setup({
 	mapping = cmp.mapping.preset.insert({
 		["<C-d>"] = cmp.mapping.scroll_docs(-4),
 		["<C-f>"] = cmp.mapping.scroll_docs(4),
-		["<C-Space>"] = cmp.mapping.complete({
-			behavior = cmp.ContextReason.Auto,
-			select = true,
-		}),
+		["<C-Space>"] = cmp.mapping(cmp.mapping.complete(), { "i", "c" }),
 		["<CR>"] = cmp.mapping.confirm({
 			behavior = cmp.ConfirmBehavior.Replace,
 			select = true,
