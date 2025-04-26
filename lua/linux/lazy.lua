e512f05ba4c8f0f1938316452c576009b6d89ce8
Dennis Persaud <dpersaud247@gmail.com>
2025-04-26 17:40:00 -0400

    Added DAP

--- lua/linux/lazy.lua
+++ lua/linux/lazy.lua
@@ -1,17 +1,17 @@
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+	local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+	local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+	if vim.v.shell_error ~= 0 then
+		vim.api.nvim_echo({
+			{ "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+			{ out, "WarningMsg" },
+			{ "\nPress any key to exit..." },
+		}, true, {})
+		vim.fn.getchar()
+		os.exit(1)
+	end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -23,31 +23,31 @@ vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
 require("lazy").setup({
-  spec = {
-    -- import your plugins
-    { import = "plugins" },
-    { import = "plugins.cmp" },
-    { import = "plugins.cmp.lsp" },
-	{ import = "plugins.cmp.linter" },
-	{ import = "plugins.cmp.formatter" },
-	{ import = "plugins.ai" },
-	{ import = "plugins.ui" },
+	spec = {
+		-- import your plugins
+		{ import = "plugins" },
+		{ import = "plugins.cmp" },
+		{ import = "plugins.cmp.lsp" },
+		{ import = "plugins.cmp.linter" },
+		{ import = "plugins.cmp.formatter" },
+		{ import = "plugins.cmp.dap" },
+		{ import = "plugins.ai" },
+		{ import = "plugins.ui" },
 
-	-- Magma Jupyter Notebook Client
-	-- { "dccsillag/magma-nvim", build = ":UpdateRemotePlugins" },
+		-- Magma Jupyter Notebook Client
+		-- { "dccsillag/magma-nvim", build = ":UpdateRemotePlugins" },
 
-	-- -- Miscellaneous
-	-- "mbbill/undotree",
-	-- "lewis6991/gitsigns.nvim",
-	-- "udalov/kotlin-vim",
+		-- -- Miscellaneous
+		-- "mbbill/undotree",
+		-- "lewis6991/gitsigns.nvim",
+		-- "udalov/kotlin-vim",
 
-	-- -- Theme
-	{ "ajmwagar/vim-deus" },
-
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "deus" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
+		-- -- Theme
+		{ "ajmwagar/vim-deus" },
+	},
+	-- Configure any other settings here. See the documentation for more details.
+	-- -- colorscheme that will be used when installing plugins.
+	install = { colorscheme = { "deus" } },
+	-- automatically check for plugin updates
+	checker = { enabled = true },
 })
--- lua/plugins/cmp/dap/dap.lua
+++ lua/plugins/cmp/dap/dap.lua
@@ -1,16 +1,71 @@
 return {
-    {
-        "mfussenegger/nvim-dap",
-        "jayp0521/mason-nvim-dap.nvim",
-        "leoluz/nvim-dap-go",
-        "theHamsta/nvim-dap-virtual-text",
-        {
-            "rcarriga/nvim-dap-ui",
-            config = function()
-                require('dapui').setup()
-            end,
-            dependencies = { "nvim-neotest/nvim-nio" }
-        },
-    },
-}
+	{
+		"mfussenegger/nvim-dap",
+		"jayp0521/mason-nvim-dap.nvim",
+		"leoluz/nvim-dap-go",
+		"theHamsta/nvim-dap-virtual-text",
+		dependencies = {
+			"rcarriga/nvim-dap-ui",
+			dependencies = {
+				"nvim-neotest/nvim-nio",
+			},
+		},
+		config = function()
+			local dap = require("dap")
+			require("dapui").setup()
+			vim.keymap.set("n", "<F5>", function()
+				dap.continue()
+			end)
 
+			vim.keymap.set("n", "<F10>", function()
+				dap.step_over()
+			end)
+
+			vim.keymap.set("n", "<F11>", function()
+				dap.step_into()
+			end)
+
+			vim.keymap.set("n", "<F12>", function()
+				dap.step_out()
+			end)
+
+			vim.keymap.set("n", "<Leader>dt", function()
+				dap.toggle_breakpoint()
+			end)
+
+			vim.keymap.set("n", "<Leader>dB", function()
+				dap.set_breakpoint()
+			end)
+
+			vim.keymap.set("n", "<Leader>dp", function()
+				dap.set_breakpoint(nil, nil, vim.fn.input("Log point message: "))
+			end)
+
+			vim.keymap.set("n", "<Leader>dr", function()
+				dap.repl.open()
+			end)
+
+			vim.keymap.set("n", "<Leader>dl", function()
+				dap.run_last()
+			end)
+
+			vim.keymap.set({ "n", "v" }, "<Leader>dh", function()
+				require("dap.ui.widgets").hover()
+			end)
+
+			vim.keymap.set({ "n", "v" }, "<Leader>dp", function()
+				require("dap.ui.widgets").preview()
+			end)
+
+			vim.keymap.set("n", "<Leader>df", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.frames)
+			end)
+
+			vim.keymap.set("n", "<Leader>ds", function()
+				local widgets = require("dap.ui.widgets")
+				widgets.centered_float(widgets.scopes)
+			end)
+		end,
+	},
+}
