496cd1b3db60414a320511d2b034e0747b9ee104
Dennis Persaud <dpersaud247@gmail.com>
2025-05-15 10:51:20 -0400

    Configured Neotest

--- lua/plugins/cmp/dap/neotest.lua
+++ lua/plugins/cmp/dap/neotest.lua
@@ -1,11 +1,54 @@
 return {
-    {
-      "nvim-neotest/neotest",
-      lazy = true,
-      dependencies = {
-        "nvim-neotest/nvim-nio",
-        { "nvim-lua/plenary.nvim", lazy = true },
-        "antoinemadec/FixCursorHold.nvim",
-      }
-    }
+	{
+		"nvim-neotest/neotest",
+
+		dependencies = {
+			"nvim-neotest/nvim-nio",
+			"nvim-lua/plenary.nvim",
+			"antoinemadec/FixCursorHold.nvim",
+			"marilari88/neotest-vitest",
+			"nvim-neotest/neotest-jest",
+			"nvim-neotest/neotest-go",
+			"olimorris/neotest-phpunit",
+			"alfaix/neotest-gtest",
+			"nvim-treesitter/nvim-treesitter",
+		},
+		keys = {
+			{ "<leader>tr", "<cmd>Neotest run<cr>" },
+			{ "<leader>ti", "<cmd>Neotest output<cr>" },
+			{ "<leader>ts", "<cmd>Neotest summary<cr>" },
+			{ "<leader>ta", "<cmd>lua require('neotest').run.run({suite = true })<cr>" }, -- Run Test Suite
+		},
+		config = function()
+			-- get neotest namespace (api call creates or returns namespace)
+			local neotest_ns = vim.api.nvim_create_namespace("neotest")
+			vim.diagnostic.config({
+				virtual_text = {
+					format = function(diagnostic)
+						local message =
+							diagnostic.message:gsub("\n", " "):gsub("\t", " "):gsub("%s+", " "):gsub("^%s+", "")
+						return message
+					end,
+				},
+			}, neotest_ns)
+			require("neotest").setup({
+				adapters = {
+					-- require("neotest-gtest").setup({}),
+					-- require("neotest-python"),
+					-- require("neotest-plenary"),
+					require("neotest-go"),
+					require("neotest-vitest"),
+					require("neotest-phpunit"),
+					require("neotest-jest")({
+						jestCommand = "npm test --",
+						jestConfigFile = "custom.jest.config.ts",
+						env = { CI = true },
+						cwd = function(path)
+							return vim.fn.getcwd()
+						end,
+					}),
+				},
+			})
+		end,
+	},
 }
