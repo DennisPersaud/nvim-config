34f91e7e07ccec52ba63c1438c7c04dfebd2f793
Dennis Persaud <dpersaud247@gmail.com>
2025-05-25 23:35:48 -0400

    Added hipatterns to mini.nvim

--- lua/plugins/cmp/mini.nvim.lua
+++ lua/plugins/cmp/mini.nvim.lua
@@ -4,6 +4,19 @@ return {
 		version = false,
 		config = function()
 			require("mini.icons").setup() -- use default config
+			local hipatterns = require("mini.hipatterns")
+			hipatterns.setup({
+				highlighters = {
+					-- Highlight standalone 'FIXME', 'HACK', 'TODO', 'NOTE'
+					fixme = { pattern = "%f[%w]()FIXME()%f[%W]", group = "MiniHipatternsFixme" },
+					hack = { pattern = "%f[%w]()HACK()%f[%W]", group = "MiniHipatternsHack" },
+					todo = { pattern = "%f[%w]()TODO()%f[%W]", group = "MiniHipatternsTodo" },
+					note = { pattern = "%f[%w]()NOTE()%f[%W]", group = "MiniHipatternsNote" },
+
+					-- Highlight hex color strings (`#rrggbb`) using that color
+					hex_color = hipatterns.gen_highlighter.hex_color(),
+				},
+			}) -- use default config
 			require("mini.surround").setup({
 				mappings = {
 					add = "sa", -- Add surrounding in Normal and Visual modes
